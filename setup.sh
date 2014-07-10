ad38338f1612388437138f17a3ee7be8a52f9403
niklasi <niklas@ingholt.com>
2014-07-11 00:06:26 +0200

    Add .gitconfig to repo.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="vim bashrc vimrc bash_profile"    # list of files/folders to symlink in homedir
+files="vim bashrc vimrc bash_profile gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
