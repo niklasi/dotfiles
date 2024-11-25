c49c0fb439ae2d0d223253d1a6a2518ead20b369
Niklas Ingholt <niklas@ingholt.com>
2024-11-25 22:37:09 +0100

    feat: add markview plugin to neovim

--- config/nvim/lua/niklasi/plugins.lua
+++ config/nvim/lua/niklasi/plugins.lua
@@ -47,6 +47,17 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+
   'MDeiml/tree-sitter-markdown',
   {
     'benmills/vimux',
