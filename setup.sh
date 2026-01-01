c5e13275450394cd1a13ac975e523b3da9f3eff0
Niklas Ingholt <niklas@ingholt.com>
2026-01-01 23:45:33 +0100

    chore: remove unused settings from setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 ########## Variables
 
 dir=~/dotfiles                    # dotfiles directory
-files="zshrc vim vimrc gitconfig tmux.conf yabairc config/nvim config/karabiner config/alacritty ideavimrc"    # list of files/folders to symlink in homedir
+files="zshrc gitconfig tmux.conf config/nvim config/karabiner config/wezterm ideavimrc hammerspoon"    # list of files/folders to symlink in homedir
 
 ##########
 
